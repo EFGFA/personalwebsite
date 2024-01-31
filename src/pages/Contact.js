2d2cd9932b92cbb1663a09c457f28d669252f563
ZihengHuo <huo1010john@gmail.com>
2024-01-30 19:04:16 -0500

    update

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
     <>
     <h1 className={styles.bigblue}>Contact Me</h1>
     <h1 className={styles.bigblue}>Contact Me</h1>
-    <img src="https://i.stack.imgur.com/SBv4T.gif" alt="this slowpoke moves"  width="250" />
+    <img src="own web\my-react-app\public\SBv4T.gif" alt="this slowpoke moves"  width="250" />
 
     </>
     

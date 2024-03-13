485af03fdac63270a8325bd78fe433051b3cfa51
ZihengHuo <huo1010john@gmail.com>
2024-03-13 13:52:21 -0400

    renew the first row version

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import'./App.css';
-import Navbar from './components/Navbar';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
 
     <BrowserRouter>
-      <Navbar />
       <Routes>
         <Route path ='/' exact />
       </Routes>
